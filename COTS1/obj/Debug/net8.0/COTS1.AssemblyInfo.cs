5c20e7c1c90f52f5514760357a80bfa00b649bba
Ngo_Nhat_Huy <ngonhathuy501@gmail.com>
2024-09-19 23:17:22 +0700

    fixbug 2

--- COTS1/obj/Debug/net8.0/COTS1.AssemblyInfo.cs
+++ COTS1/obj/Debug/net8.0/COTS1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COTS1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+376886a94eaa7fdb6c4f6dd1a3387c6e5677fae0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cc8668e7e7ca6c00893d9733911fe807dfa6354")]
 [assembly: System.Reflection.AssemblyProductAttribute("COTS1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COTS1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
