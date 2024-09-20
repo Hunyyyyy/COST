99bc04babb20dd9bbc27f7541c338c65b9633fd4
Ngo_Nhat_Huy <ngonhathuy501@gmail.com>
2024-09-20 07:47:13 +0700

    d

--- COTS1/obj/Debug/net8.0/COTS1.AssemblyInfo.cs
+++ COTS1/obj/Debug/net8.0/COTS1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COTS1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7afbd7c28813545ffd13defdc89fd04b38f3b4f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387bbfc5830cb95ed29ff7f2e838b9d4616f9fee")]
 [assembly: System.Reflection.AssemblyProductAttribute("COTS1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COTS1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
